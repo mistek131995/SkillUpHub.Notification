5d37505a7c68debec655c72815317a80672278ae
Юрий Кузнецов <omega131995@outlook.com>
2024-11-10 16:54:29 +0700

    Настройка аутентификации и CORS

--- SkillUpHub.Notification/Program.cs
+++ SkillUpHub.Notification/Program.cs
@@ -1,12 +1,60 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using SkillUpHub.Notification;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region Настройка аутентификации
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "SkillHub.Auth",
+        ValidAudience = "SkillHub.Services",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("SecretKey").Value)) // Ваш секретный ключ
+    };
+
+    // Важно: поддержка gRPC Web
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
+
+#endregion
+
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.WithOrigins("http://localhost:5173")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+}));
 
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 app.UseRouting();
-app.MapHub<NotificationHub>("/notificationHub");
+app.MapHub<NotificationHub>("/notificationHub").RequireAuthorization();
 
 app.Run();
